69614a1d5f5760693c2366fa7fd6e4c9e5aba6de
Aryeh Harris <harryttd@gmail.com>
2017-02-06 12:15:00 -0500

    Add skeleton for different turn directions in reducer.

--- browser/reducers/players.js
+++ browser/reducers/players.js
@@ -1,5 +1,6 @@
 import allPlayers from '../game/players';
 import world from '../game/world';
+import { turnLeft, turnRight, turnUp, turnDown } from '../game/gamePlayFunctions';
 // import socket from '../socket';
 
 /*----------  INITIAL STATE  ----------*/
@@ -9,7 +10,12 @@ const initialState = allPlayers;
 const RECEIVE_PLAYERS = 'RECEIVE_PLAYERS';
 const REMOVE_ALL_PLAYERS = 'REMOVE_ALL_PLAYERS';
 const SET_PLAYER_ID = 'SET_PLAYER_ID';
+
 const UPDATE_PLAYER = 'UPDATE_PLAYER';
+const TURN_PLAYER_LEFT = 'TURN_PLAYER_LEFT';
+const TURN_PLAYER_RIGHT = 'TURN_PLAYER_RIGHT';
+const TURN_PLAYER_UP = 'TURN_PLAYER_UP';
+const TURN_PLAYER_DOWN = 'TURN_PLAYER_DOWN';
 
 /*----------  ACTION CREATORS  ----------*/
 export const receivePlayers = (players) => ({
@@ -26,6 +32,16 @@ export const setPlayerId = (playerId, index) => ({
 export const removeAllPlayers = () => ({
   type: REMOVE_ALL_PLAYERS
 });
+
+
+export const turnPlayer = (direction, player) => {
+  let turn;
+  if (direction === "Left") turn = TURN_PLAYER_LEFT;
+  if (direction === "Right") turn = TURN_PLAYER_RIGHT;
+  if (direction === "Up") turn = TURN_PLAYER_UP;
+  if (direction === "Down") turn = TURN_PLAYER_DOWN;
+  return { turn, player };
+};
 
 export const updatePlayer = (linearVelocity, player) => ({
   type: UPDATE_PLAYER,
@@ -49,6 +65,16 @@ export default (state = initialState, action) => {
         }
         return bike;
       });
+
+      case TURN_PLAYER_LEFT:
+        return;
+      case TURN_PLAYER_RIGHT:
+        return;
+      case TURN_PLAYER_UP:
+        return;
+      case TURN_PLAYER_DOWN:
+        return;
+
     case UPDATE_PLAYER:
       console.log("ACTION", action);
       return state.map((player, index) => {
