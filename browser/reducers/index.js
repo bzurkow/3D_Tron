df57c9df6e43cb31b56b408222698cd153cc7698
Reico Lee <leereico@gmail.com>
2017-02-11 06:30:26 -0500

    adding stuff for chat features

--- browser/reducers/index.js
+++ browser/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import players from './players';
 import mainPlayer from './mainPlayer';
 import gameState from './gameState';
+import messags from './messages';
 
 export default combineReducers({
   mainPlayer,
   players,
-  gameState
+  gameState,
+  messages
 });
